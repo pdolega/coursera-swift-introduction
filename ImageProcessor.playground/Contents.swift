fcf94c15811dd5f9e6aedb072e67bf5b2dd9d49c
Pawel Dolega PWD <pdolega@Pawels-MacBook-Pro.local>
2016-06-16 01:24:21 +0200

    Initial version with classes

--- ImageProcessor.playground/Contents.swift
+++ ImageProcessor.playground/Contents.swift
@@ -6,45 +6,55 @@ let image = UIImage(named: "sample")
 
 // Process the image!
 
-var imageToModify = RGBAImage(image: image!)!
 
-let width = imageToModify.height
-let height = imageToModify.width
-
-let totalRed = 0
-let totalGreen = 0
-let totalBlue = 0
-
-func executeForEachPixel(image: RGBAImage, logic: (index: Int, pixel: Pixel) -> Void) {
-    for index in 0..<image.width * image.height {
-        logic(index: index, pixel: image.pixels[index])
+class Filter {
+    func filter(inout image: RGBAImage) -> Void {
+        
     }
-}
-
-var colorSums = (0, 0, 0)
-executeForEachPixel(imageToModify) { index, pixel in
-    colorSums = (colorSums.0 + Int(pixel.red), colorSums.1 + Int(pixel.green), colorSums.2 + Int(pixel.blue))
-}
-
-let totalPixelCount = width * height
-let colorAvgs = (colorSums.0 / totalPixelCount, colorSums.1 / totalPixelCount, colorSums.2 / totalPixelCount)
-let redAvg = colorAvgs.0
-let greenAvg = colorAvgs.1
-let blueAvg = colorAvgs.2
-
-print(colorAvgs)
-
-executeForEachPixel(imageToModify) { index, pixel in
-    let diff = Int(pixel.red) - redAvg
     
-    if diff > 0 {
-        var pixelToChange = pixel
-        let newRed = UInt8(min(255, redAvg + 3 * diff))
-        pixelToChange.red = newRed
-        imageToModify.pixels[index] = pixelToChange
+    func executeForEachPixel(inout image: RGBAImage, logic: (index: Int, pixel: Pixel) -> Void) {
+        for index in 0..<image.width * image.height {
+            logic(index: index, pixel: image.pixels[index])
+        }
     }
 }
 
-let newImage = imageToModify.toUIImage()!
+class FilterProcessor {
+    func process(srcImage: UIImage, filters: Filter...) -> UIImage {
+        var imageToModify = RGBAImage(image: srcImage)!
 
+        filters.forEach {
+            $0.filter(&imageToModify)
+        }
+        
+        return imageToModify.toUIImage()!
+    }
+}
 
+class AmplifyRedFilter: Filter {
+    override func filter(inout image: RGBAImage) -> Void {
+        var colorSums = (0, 0, 0)
+
+        executeForEachPixel(&image) { index, pixel in
+            colorSums = (colorSums.0 + Int(pixel.red), colorSums.1 + Int(pixel.green), colorSums.2 + Int(pixel.blue))
+        }
+        
+        let totalPixelCount = image.width * image.height
+        let colorAvgs = (colorSums.0 / totalPixelCount, colorSums.1 / totalPixelCount, colorSums.2 / totalPixelCount)
+        let redAvg = colorAvgs.0
+        
+        executeForEachPixel(&image) { index, pixel in
+            let diff = Int(pixel.red) - redAvg
+            
+            if diff > 0 {
+                var pixelToChange = pixel
+                let newRed = UInt8(min(255, redAvg + 3 * diff))
+                pixelToChange.red = newRed
+                image.pixels[index] = pixelToChange
+            }
+        }
+    }
+}
+
+let processor = FilterProcessor()
+let processedImage = processor.process(image!, filters: AmplifyRedFilter())
